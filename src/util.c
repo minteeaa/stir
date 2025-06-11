afe6ce42747e9e22a123b15cda3432f2195631b8
mintea <minteaseu@outlook.com>
2025-06-11 15:31:10 -0400

    fix: use obs log for error

--- src/util.c
+++ src/util.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <obs-module.h>
 
+#include "plugin-support.h"
+
 char *concat(const char *str1, const char *str2)
 {
 	size_t len1 = strlen(str1);
@@ -9,7 +11,7 @@ char *concat(const char *str1, const char *str2)
 
 	char *result = (char *)bzalloc(len1 + len2 + 1);
 	if (result == NULL) {
-		perror("Failed to allocate memory");
+		obs_log(LOG_ERROR, "failed to allocate memory for concat job");
 		return NULL;
 	}
 
